6b78abcb58a18689074f65cac7fddb6ea4dff642
Anthony Aguilar <aguilaranthony.ve@gmail.com>
2021-02-12 15:13:14 -0300

    creacion de Dados

---
+++ 2_simular_dado/src/main/java/com/example/demo/Player/Dice.java
@@ -0,0 +1,10 @@
+package com.example.demo.Player;
+
+public class Dice {
+
+    private int sides;
+
+    public int roll(){
+        return 1;
+    }
+}
---
+++ 2_simular_dado/src/main/java/com/example/demo/Player/Player.java
@@ -0,0 +1,9 @@
+package com.example.demo.Player;
+
+public class Player {
+
+    private Dice dice;
+    public boolean play(){
+        return false;
+    }
+}
